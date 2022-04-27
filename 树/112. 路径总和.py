901fd7e447edc29658c735e775eb9742e6dac211
liupengfei66 <liupengfei66@163.com>
2022-04-27 15:43:48 +0800

    add

--- 树/112. 路径总和.py
+++ 树/112. 路径总和.py
@@ -10,30 +10,20 @@ class TreeNode:
 
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        if not root:
-            return False
-
-        def traversal(root, cnt):
-            # 到达叶结点
+        def traversal(root, total):
+            if not root:
+                return False
+            total += root.val
             if not root.left and not root.right:
-                if cnt == 0:
+                if total == targetSum:
                     return True
-                else:
-                    return False
-
-            if root.left:
-                cnt -= root.left.val  # 递归，cnt代入下层处理
-                if traversal(root.left, cnt):
-                    return True
-                cnt += root.left.val  # 回溯，cnt回归原样
-            if root.right:
-                cnt -= root.right.val
-                if traversal(root.right, cnt):
-                    return True
-                cnt += root.right.val
-            return False
-
-        return traversal(root, targetSum - root.val)
+            left = traversal(root.left, total)
+            right = traversal(root.right, total)
+            if left or right:
+                return True
+            else:
+                return False
+        return traversal(root, 0)
 
 # 迭代，前序遍历寻找路径和，栈里的元素是一个pair对，存储了当前的结点以及路径和
 class Solution:
@@ -43,12 +33,11 @@ class Solution:
         stack = [(root, root.val)]
         while stack:
             curr = stack.pop()
-            node, val = curr
-            if not node.left and not node.right and val==targetSum:
+            node, total = curr
+            if not node.left and not node.right and total == targetSum:
                 return True
             if node.right:
-                stack.append((node.right, val+node.right.val))
+                stack.append((node.right, total+node.right.val))
             if node.left:
-                stack.append((node.left, val+node.left.val))
-
+                stack.append((node.left, total+node.left.val))
         return False
---
+++ 树/513. 找树左下角的值.py
@@ -0,0 +1,47 @@
+# https://leetcode-cn.com/problems/find-bottom-left-tree-value/
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# 层序遍历，
+# 可以每次从右开始遍历到左，依次记录值，那么保留的就是最后一层最左边的
+# 也可以从左到右遍历，但是每次只记录第一个值
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        queue = collections.deque([root])
+        res = root.val
+        while queue:
+            n = len(queue)
+            for _ in range(n):
+                node = queue.popleft()
+                res = node.val
+                if node.right:
+                    queue.append(node.right)
+                if node.left:
+                    queue.append(node.left)
+        return res
+
+# 解法2，迭代
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        self.max_depth = 1
+        self.res = root.val
+        def traversal(root, depth):
+            if not root:
+                return
+            # 叶结点
+            if not root.left and not root.right:
+                # 前序遍历，只有最左边的节点可能被执行
+                if depth > self.max_depth:
+                    self.res = root.val
+                    self.max_depth = depth
+            # depth作为参数，实际蕴含了回溯的思想
+            # 使得高度可以随着递归而增加，随着返回而减少
+            traversal(root.left, depth+1)
+            traversal(root.right, depth+1)
+
+        traversal(root, 1)
+        return self.res
